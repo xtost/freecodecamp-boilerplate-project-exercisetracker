b9992419200c0c6881073a51a656019f351466f6
Your Name <you@example.com>
2021-10-08 15:38:46 +0200

    done

---
+++ model.js
@@ -0,0 +1,330 @@
+
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const exercisesSubSchema = new Schema(
+    {
+       // id: { type: String, required: true},
+        description: { type: String, required: true},
+        duration: { type: Number, required: true},
+        date: String
+    }
+    , { _id : false }
+);
+
+const userSchema = new Schema(
+    {
+        username: {type: String, required: true}
+    }
+    
+)
+
+/* This one creates an _id in the log entry, not suitable 
+   according to the specs, use the one below instead
+
+   see: {_id: false}
+   
+const userExercisesSchema = new Schema(
+    {
+        username: {type: String, required: true},
+        count: Number,
+        log: [{
+                description: String,
+                duration: Number,
+                date: Date
+        }]
+    }
+)
+
+*/
+
+
+/* https://stackoverflow.com/questions/17254008/stop-mongoose-from-creating-id-property-for-sub-document-array-items */
+const userExercisesSchema = new Schema(
+    {
+        username: {type: String, required: true},
+        count: Number,
+        log: [exercisesSubSchema]
+    } 
+)
+
+
+let User = mongoose.model('User',userSchema);
+let UserExercises = mongoose.model('UserExercises',userExercisesSchema);
+
+module.exports = {
+
+    mongoDbConnect: function() {
+        mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    },
+
+    createANewUser: function (input, done) {
+    
+        User.findOne({ username: input }, (err, data) => {
+            if (err || data === null) {
+                let userDoc = new User({username: input});
+                userDoc.save((err,data) => err? done(err,'Path `username` is required.'): done(null,data));
+            } else {
+                done(null,data);
+            }
+        });
+/*
+        let userDoc = new User({username: input});
+        userDoc.save((err,data) => err? done(err): done(null,data));
+*/
+    },
+
+    addExercises: function (_id, description, duration, date, done) {
+
+        let dateval =new Date(date+'T00:00:00').toDateString();
+        User.findById(_id, (err, data) => {
+            if (err) {
+                done(err);
+            } else {
+                UserExercises.findOne({ username: data.username}, (erruserexerc, userexerc) => {
+                    if (erruserexerc || userexerc === null) {
+
+                        //var formatDate = new Date(date);
+                        //var options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+                        /* https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date */
+                        // console.log(formatDate.toLocaleDateString("en-US", options));
+                       /* https://stackoverflow.com/questions/67177522/remove-comma-from-date-which-is-generated-from-intl-datetimeformat */
+                        //formatDate.replace(/, /g, " "); //  a simple replace removes the comma
+                        //console.log(formatDate);
+                        let userExercisesDoc = new UserExercises(
+                            {
+                                username: data.username,
+                                count: 1,
+                                log: [{description: description,
+                                        duration: duration,
+                                        date: dateval}]
+                            });
+                            userExercisesDoc.save((erruserexerc,userexerc) => erruserexerc? done(erruserexerc): done(null,userexerc));
+                    } else {
+                        userexerc['log'].push({
+                            description: description,
+                            duration: duration,
+                            date: dateval
+                        });
+                        userexerc['count'] = userexerc['count'] + 1;
+                        userexerc.save((erruserexerc, userexerc) => (erruserexerc? done(erruserexerc) : done(null, userexerc)));
+                    }
+
+                });
+
+            }
+        })
+    }, 
+
+    getUserExerciseLog: function (_id, from, to, limit, done) {
+
+     if (from)
+        frompar =  new Date(new Date(from).toDateString());
+    else 
+        topar = null
+     if (to)
+        topar =  new Date(new Date(to).toDateString());
+     else 
+        topar = null   
+
+
+        User.findById(_id, (err, data) => {
+            if (err === null && data!== null) {
+
+                UserExercises
+                .findOne({"username": data.username, "log.duration": 1}).lean()
+                    .exec(function(err2, data2)
+                        {
+                            /* findOne.lean() is key to be able to check json */
+                            logs = data2.log;
+                            let i=0;
+                            let logsadded=0;
+                            let newlogs = [];
+                            let loopended = false;
+                            while (i<logs.length && !loopended) {
+                                log = logs[i];
+                                let fromok = false;
+                                let took = false;
+                                if (from) {
+                                    if (new Date(log.date).getTime()>=frompar.getTime()) {
+                                        fromok=true;
+                                    }
+                                } else {
+                                    fromok=true;
+                                }
+                                if (to) {
+                                    if (new Date(log.date).getTime()<=topar.getTime()) {
+                                        took=true;
+                                    }
+                                } else {
+                                    took=true;
+                                }
+                                if (fromok && took && 
+                                        ((!limit) || (limit && logsadded<limit))) {
+                                    newlogs[logsadded] = log;
+                                    logsadded=logsadded+1;
+                                }
+                                if (limit && logsadded >= limit) loopended = true;
+
+                                i++;
+
+                            }
+
+                            data2.log = newlogs;
+
+
+                            if(err2) done(err2) ;
+                            else done(null,data2);
+
+                        }
+                    )
+/*
+                .aggregate([
+                    {$match: { username: data.username }},
+                    { $project: {
+                            log: {$filter: {
+                                    input: '$log',
+                                    as: 'item',
+                                    cond: {$eq: ['$$item.description', 'asdf']}
+                                    }
+                                    },
+                                }},
+                    { $project: {
+                            log: {$filter: {
+                                    input: '$log',
+                                    as: 'item',
+                                    cond: {$eq: ['$$item.duration', 12]}
+                                 }}
+                        }}
+                        
+                        ,
+                    { $project: {
+                            log: {$filter: {
+                                    input: '$log',
+                                    as: 'item',
+                                    cond: {$gte: ['$dateFromString: {$$item.date}', (from)?(new Date('2020-09-20T07:00:00.000Z')):null]}
+                                 }}
+                        }}
+                    ], (err2,data2) => (err2)?done(err2):done(null,data2))
+                */
+               
+                    
+                        /*
+                     ,
+                        { $project: {
+                            log: {$filter: {
+                                    input: '$log',
+                                    as: 'item',
+                                    cond: {$lte: [Date('$$item.date'), (topar)?topar:null]}
+                                 }}
+                        }}*/
+                   
+             
+                
+                
+                
+                
+                /*
+                find({username: data.username})
+                    .exec(function(err2, data2)
+                        {
+
+                            if(err2) done(err2) ;
+                            else done(null,data2);
+
+                        }
+                    )
+
+*/
+
+            } else if (data!== null) {
+                done(err);
+            } else {
+                done(err);
+            }
+
+
+                /*
+                //it would also work with data['username']
+                UserExercises.findOne({ username: data.username }, (err2, data2) => (err2 ? done(err2) : done(null, data2)));
+                */
+
+                /*
+                UserExercises
+                .aggregate([
+                    {$match: { username: data.username }},
+                    {$unwind: "$log"},
+                    {$match: {"log.description": "asdf"}},
+                    {$group: {"_id": "$_id",}}
+                ])
+
+                .exec((err2, data2) => (err2 ? done(err2) : done(null, data2)));
+*/
+
+
+
+/*
+.aggregate([
+    {$match: { username: data.username }},
+    { $project: {
+            log: {$filter: {
+                    input: '$log',
+                    as: 'item',
+                    cond: {$eq: ['$$item.description', 'asdf']}
+                    }
+                    },
+                }},
+    { $project: {
+            log: {$filter: {
+                    input: '$log',
+                    as: 'item',
+                    cond: {$eq: ['$$item.duration', 12]}
+                 }}
+        }}
+        
+        ,
+    { $project: {
+            log: {$filter: {
+                    input: '$log',
+                    as: 'item',
+                    cond: {$gte: ['new Date($$item.date)', (from)?(new Date('2020-09-20T07:00:00.000Z').toISOString()):null]}
+                 }}
+        }}
+
+        */
+        /*
+     ,
+        { $project: {
+            log: {$filter: {
+                    input: '$log',
+                    as: 'item',
+                    cond: {$lte: [Date('$$item.date'), (topar)?topar:null]}
+                 }}
+        }}*/
+   /*
+    ])
+
+.exec((err2, data2) => (err2 ? done(err2) : done(null, data2)));
+*/
+
+
+
+
+            
+        })
+
+        /*
+            Person.find({ favoriteFoods: foodToSearch })
+            .sort({ name: 'asc'})
+            .limit(2)
+            .select('-age')
+            .exec((err, data) => (err ? done(err) : done(null, data)));
+        */
+
+    },
+
+    getOnlyOneUser: function (input, done) {
+        User.findOne({ username: input }, (err, data) => (err ? done(err) : done(null, data)));
+    }
+
+}
--- server.js
+++ server.js
@@ -1,15 +1,60 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+var bodyparser = require ('body-parser');
+const { createANewUser, mongoDbConnect, addExercises, getUserExerciseLog } = require('./model')
 require('dotenv').config()
+
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyparser.urlencoded({extended: false}));
+
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+mongoDbConnect();
 
+app.post('/api/users', (req, res,next)=>{
+  let user = req.body.username;
+  createANewUser(user, (err,data)=> {
+    if (err) res.send(err.message);
+    res.json(data);
+  });
+
+});
+
+app.post('/api/users/:_id/exercises', (req,res,next)=> {
+  let _id = req.params._id;
+  let description = req.body.description;
+  let duration = req.body.duration;
+  let date = req.body.date;
+
+  addExercises(_id, description, duration, date, (err,data) => {
+    if (err) res.send(err.message);
+    res.json(data);  
+  })
+
+
+});
+
+app.get('/api/users/:_id/logs', (req,res) => {
+  let _id = req.params._id;
+  let from = req.query.from;
+  let to = req.query.to;
+  let limit = req.query.limit;
+
+  getUserExerciseLog(_id, from, to, limit, (err, data)=> {
+
+    if (err) 
+      res.send(err.message);
+    else
+      res.json(data); 
+  })
+
+}) 
 
 
 
